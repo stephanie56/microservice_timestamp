56cc78725a28815cac3a8708c2d9ad61e8635592
Communications <Communications>
2017-07-18 17:11:35 -0400

    Update

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res){
-  console.log('Hello World!!');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
 });
 
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000');
+
+const user_input = "1450137600";
+app.get('/user_input', (req, res) => {
+  res.send('hey you');
+});
+
+app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}.`);
 });
