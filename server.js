1113e2ed766068ceaddf3a2d4e5cd164f3b03e5d
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-07-26 23:14:22 -0400

    Created express server

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/views'));
-
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-
-
-const user_input = "1450137600";
-app.get('/user_input', (req, res) => {
-  res.send('hey you');
-});
-
-app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}.`);
-});
+app.listen(PORT);
