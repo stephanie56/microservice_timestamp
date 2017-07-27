24ce166cfc8ed9f61cca1a444f8a096ed27c194d
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-07-26 23:58:06 -0400

    Get requested id

--- server.js
+++ server.js
@@ -7,4 +7,21 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+
+app.get('/:id', (req, res) => {
+  const userInput = req.params.id;
+  if (!isNaN(userInput)){
+    res.send({
+      "unix": userInput,
+      "natural": new Date(userInput).toString()
+    });
+  }
+  else {
+    res.send({
+      "unix": null,
+      "natural": null
+    });
+  }
+});
+
 app.listen(PORT);
