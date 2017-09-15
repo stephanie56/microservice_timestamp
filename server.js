f26be69db7a66c673ed30bfb20bbe192a4aaa437
Stephanie Zeng <stephanie56@users.noreply.github.com>
2017-09-15 12:00:40 -0400

    Added helper functions

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
+
+/** HELPER FUNCTIONS **/
+
+/** NUMBER_TO_DATE
+input: Number (unix date)
+expected output: String (natural date)
+**/
+function NUMBER_TO_DATE(num){
+      return naturalDateString;
+}
+
+/** DATESTRING_TO_UNIX
+input: String 
+expected output: Number (unix date)
+**/
+function DATESTRING_TO_UNIX(datestring){
+      return unixDate;
+}
+
 // static route
 app.get('/', (req, res) => {
   res.sendFile(join(VIEWS, "index.html"));
@@ -23,26 +42,21 @@ app.get('/:param', (req, res) => {
    "natural": null
   };
   
-  // Condition: is unserInput or Date.parse(userInput) a Number?
-  // 1. userInput is a Number: (userInput) => NUMBER_TO_DATE(userInput)
-  // 2. userInput is not a Number:
-  //    a. Date.parse(userInput) is a Number: (userInput) => unix(userInput)
-  //    b. Date.parse(userInput) is not a Number: send null
-  
-  // userInput is a Number: 
+  // CASE 1: userInput is a Number: NUMBER_TO_DATE(userInput)
   if (!isNaN(userInput)) {
     POST.unix = userInput;
     POST.natural = NUMBER_TO_DATE(userInput);
-  }
-  
-  // userInput is not a Number && Date.parse(userInput) is a Number: 
-  if (isNaN(userInput) && !isNaN(Date.parse(userInput))) {
+    res.send(POST);
+  } 
+  // CASE 2: userInput is not a Number BUT Date.parse(userInput) is a Number: DATESTRING_TO_UNIX(userInput)
+  else if (isNaN(userInput) && !isNaN(Date.parse(userInput))) {
     POST.unix = DATESTRING_TO_UNIX(userInput);
-    POST.natural = userInput;;
+    POST.natural = userInput;
+    res.send(POST);
+  } 
+  else {
+    res.send(POST);
   }
-      
-  res.send(POST);
-      
 });
 
 app.listen(PORT);
