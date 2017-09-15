40b24a8464ab64c4c9d8a30c4110412023f40232
Stephanie Zeng <stephanie56@users.noreply.github.com>
2017-09-15 11:14:08 -0400

    Refactored code - Server logic

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const path = require('path');
 
-const app = express();
+const path = require('path'),
+      join = path.join,
+      resolve = path.resolve,
+      VIEWS = join(__dirname, "views");
+
 const PORT = process.env.PORT || 3000;
 
+const express = require('express');
+const app = express();
+
+// static route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(join(VIEWS, "index.html"));
 });
 
+// dynamic route
 app.get('/:param', (req, res) => {
+  // POST result based on req.url
   const userInput = req.params.param;
+  const POST = {};
+  
+  // Condition: typeof unserInput
+  // 1. userInput is a Number: (userInput) => natural(userInput)
+  // 2. userInput is not a Number:
+  //    a. Date.parse(userInput) is a Number: (userInput) => unix(userInput)
+  //    b. Date.parse(userInput) is not a Number: send null
+   
   if (!isNaN(userInput)) {
     res.send({
       "unix": userInput,
